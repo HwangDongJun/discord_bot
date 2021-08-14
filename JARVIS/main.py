bc33abbb55797e1c6b7a4381dcfebaeb92ed8fd0
DongjunHwang <enter031@naver.com>
2021-08-14 18:10:19 +0900

    feat: create get channel info & user info but user info is still unfinished

--- JARVIS/main.py
+++ JARVIS/main.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+from typing import Optional
+
 import discord, asyncio, os
+from discord import Embed, Member
 from discord.ext import commands
 
 import requests
@@ -22,7 +26,7 @@ async def on_member_remove(member):
 async def hello(ctx):
 	await ctx.send(f'{ctx.author.mention}님 안녕하세요!')
 
-@bot.command(aliases=['KOSPI'])
+@bot.command(aliases=['KOSPI', '코스피'])
 async def kospi(ctx, text):
 	# 주식 데이터를 그때그때 저장할 시 시간이 오래 걸리기에 다른 대안이 필요하긴함...
 	# 또한 현재 코스피만 만들둔 상태임 // 코스닥도 추가해야함
@@ -51,5 +55,35 @@ async def kospi(ctx, text):
 	else:
 		await ctx.send(f"현재 {ctx.author.mention}님이 요청하신 기업에 대한 정보는 없습니다.")
 
+@bot.command(aliases=['채널'])
+async def channel(ctx, channel: discord.TextChannel):
+	await ctx.send(f"대상채널이름: {channel.name} \n대상채널ID: {channel.id}")
 
-bot.run('비공개')
+#@bot.command(aliases=['유저', '사용자'])
+#async def user(ctx, user: discord.User):
+#	await ctx.send(f"대상유저이름: {user.name} \n대상유저ID: {user.id} \n대상유저현재활동: {user.activities}, {user.")
+
+@bot.command(aliases=['유저', '사용자'])
+async def user_info(ctx, target: Optional[Member]):
+	target = target or ctx.author
+
+	embed = Embed(title="User information",
+				  colour=target.colour,
+				  timestamp=datetime.utcnow())
+	embed.set_thumbnail(url=target.avatar_url)
+
+	fields = [("Name", str(target), True),
+			  ("ID", target.id, False),
+			  ("Bot?", target.bot, True),
+			  ("Top role", target.top_role.mention, True),
+			  ("Status", str(target.status).title(), True),
+			  ("Activity", f"{str(target.activity.type).split('.')[-1].title() if target.activity else 'N/A'} {target.activity.name if target.activity else ''}", True),
+			  ("Created at", target.created_at.strftime("%d/%m/%Y %H:%M:%S"), True),
+			  ("Joined at", target.joined_at.strftime("%d/%m/%Y %H:%M:%S"), True)]
+
+	for name, value, inline in fields:
+		embed.add_field(name=name, value=value, inline=inline)
+	
+	await ctx.send(embed=embed)
+
+bot.run('ODcyMTUxMzQzODM2NTY1NTI0.YQlsPA.wb_nyUyHr4fkrLfliR5sOC0d5Qs')
